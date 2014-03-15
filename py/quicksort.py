7b7f750344b94fde34d60d395bb6db52ef1d5bd1
Fraser Tweedale <frase@frase.id.au>
2014-03-15 12:25:58 -0700

    add in-place quicksort

--- py/quicksort.py
+++ py/quicksort.py
@@ -15,3 +15,28 @@ def _sort(xs):
     for x in xs[1:]:
         (l if x < pivot else r).append(x)
     return _sort(l) + [pivot] + _sort(r)
+
+
+def in_place(xs):
+    random.shuffle(xs)
+    return _in_place(xs, 0, len(xs) - 1)
+
+
+def _in_place(xs, lo, hi):
+    if hi <= lo:
+        return
+    i = lo
+    j = hi + 1
+    while True:
+        i += 1
+        j -= 1
+        while i < hi and xs[i] < xs[lo]:
+            i += 1
+        while j > lo and xs[j] > xs[lo]:
+            j -= 1
+        if i >= j:
+            break
+        xs[i], xs[j] = xs[j], xs[i]
+    xs[lo], xs[j] = xs[j], xs[lo]
+    _in_place(xs, lo, j - 1)
+    _in_place(xs, j + 1, hi)
--- py/test_quicksort.py
+++ py/test_quicksort.py
@@ -7,7 +7,13 @@ from . import quicksort
 class SortTestCase(unittest.TestCase):
     maxDiff = None
 
-    def test_sorts(self):
+    def test_sort(self):
         xs = range(100)
         random.shuffle(xs)
         self.assertEqual(quicksort.sort(xs), sorted(xs))
+
+    def test_in_place(self):
+        xs = range(100)
+        random.shuffle(xs)
+        quicksort.in_place(xs)
+        self.assertEqual(xs, range(100))
